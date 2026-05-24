22027ea6b271ca82af652157dbde69195949db56
nikhil kumar <nikhilkmr1725@gmail.com>
2026-05-25 00:03:31 +0530

    upto card component done

--- Second_Brain_Frontend/src/App.tsx
+++ Second_Brain_Frontend/src/App.tsx
@@ -7,12 +7,20 @@ import { ShareIcon } from "./icons/ShareIcon";
 
 function App() {
   return(
-    <>
-      <Button  startIcon={<PlusIcon size="md"/>} endIcon={<ShareIcon size="md"/>} size="md" variant="primary" text="Share"/>
-      <Button startIcon={<PlusIcon size="md"/>} endIcon={<ShareIcon size="md"/>} size="md" variant="secondary" text ="Add content"/>
+    <div className='p-4'>
+      
+      <div className="flex justify-end gap-4">
+        <Button  startIcon={<PlusIcon size="md"/>} endIcon={<ShareIcon size="md"/>} size="md" variant="primary" text="Share"/>
+        <Button startIcon={<PlusIcon size="md"/>} endIcon={<ShareIcon size="md"/>} size="md" variant="secondary" text ="Add content"/>
+      </div>
 
-      <Card />
-    </>
+      <div className="flex gap-4">
+        <Card  type="twitter" link="https://twitter.com/Interior/status/463440424141459456" title="first tweet"/>
+
+        <Card type="youtude" link="https://www.youtube.com/watch?v=nDjloeIB3Pc" title="First video"/>
+      </div>
+      
+    </div>
   )
 }
 
